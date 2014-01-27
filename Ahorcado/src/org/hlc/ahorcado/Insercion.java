6ec0416f66928ee46e2e6795e5d9a19d8cf823eb
zacr0 <zacro.suarez@gmail.com>
2014-01-27 01:47:56 +0100

    Modificacion menu Insercion
    Eliminada la opción generada.

--- Ahorcado/src/org/hlc/ahorcado/Insercion.java
+++ Ahorcado/src/org/hlc/ahorcado/Insercion.java
@@ -40,16 +40,7 @@ public class Insercion extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
+			// TODO
 		}
 		return super.onOptionsItemSelected(item);
 	}
