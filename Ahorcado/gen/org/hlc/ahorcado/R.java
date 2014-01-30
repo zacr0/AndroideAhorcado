392c438d1ee6e36e546d44f8ed9ba1571b4c333d
zacr0 <zacro.suarez@gmail.com>
2014-01-30 19:51:03 +0100

    Progresos en Insercion y Juego, layouts añadidos

--- Ahorcado/gen/org/hlc/ahorcado/R.java
+++ Ahorcado/gen/org/hlc/ahorcado/R.java
@@ -26,21 +26,56 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout=0x7f080000;
-        public static final int action_insertar=0x7f08000e;
-        public static final int action_jugar=0x7f08000f;
-        public static final int action_salir=0x7f08000d;
-        public static final int action_settings=0x7f08000c;
-        public static final int action_volver=0x7f08000b;
+        public static final int LinearLayout1=0x7f08000b;
+        public static final int a=0x7f08001b;
+        public static final int action_insertar=0x7f080031;
+        public static final int action_jugar=0x7f080032;
+        public static final int action_salir=0x7f080030;
+        public static final int action_settings=0x7f08002f;
+        public static final int action_volver=0x7f08002e;
+        public static final int b=0x7f080029;
         public static final int boton_insertar=0x7f08000a;
         public static final int boton_jugar=0x7f080005;
+        public static final int boton_menu=0x7f080006;
+        public static final int boton_nuevo_juego=0x7f08002d;
         public static final int boton_palabra=0x7f080004;
-        public static final int boton_salir=0x7f080006;
+        public static final int c=0x7f080027;
         public static final int campo_insertar=0x7f080009;
+        public static final int contador=0x7f08000d;
+        public static final int d=0x7f08001d;
+        public static final int e=0x7f080012;
+        public static final int f=0x7f08001e;
+        public static final int g=0x7f08001f;
+        public static final int h=0x7f080020;
+        public static final int i=0x7f080017;
         public static final int imageView1=0x7f080001;
+        public static final int j=0x7f080021;
+        public static final int k=0x7f080022;
+        public static final int l=0x7f080023;
+        public static final int m=0x7f08002b;
+        public static final int n=0x7f08002a;
+        public static final int o=0x7f080018;
+        public static final int p=0x7f080019;
+        public static final int palabra=0x7f08000e;
+        public static final int q=0x7f080010;
+        public static final int r=0x7f080013;
+        public static final int s=0x7f08001c;
         public static final int subtitulo=0x7f080003;
         public static final int subtitulo_insertar=0x7f080008;
+        public static final int t=0x7f080014;
+        public static final int tableRow1=0x7f08000f;
+        public static final int tableRow2=0x7f08001a;
+        public static final int tableRow3=0x7f080024;
         public static final int titulo=0x7f080002;
+        public static final int titulo_fallos=0x7f08000c;
         public static final int titulo_insertar=0x7f080007;
+        public static final int titulo_resultado=0x7f08002c;
+        public static final int u=0x7f080016;
+        public static final int v=0x7f080028;
+        public static final int w=0x7f080011;
+        public static final int x=0x7f080026;
+        public static final int y=0x7f080015;
+        public static final int z=0x7f080025;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -52,15 +87,19 @@ public final class R {
         public static final int insercion=0x7f070000;
         public static final int juego=0x7f070001;
         public static final int main=0x7f070002;
+        public static final int resultado=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int error_falta_palabra=0x7f05000d;
         public static final int error_formato_palabra=0x7f05000e;
+        public static final int resultado_ganar=0x7f050014;
+        public static final int resultado_perder=0x7f050015;
         public static final int texto_auxiliar_icono=0x7f050007;
         public static final int texto_boton_insertar=0x7f05000b;
         public static final int texto_boton_jugar=0x7f050005;
+        public static final int texto_boton_nuevo_juego=0x7f050012;
         public static final int texto_boton_palabra=0x7f050004;
         public static final int texto_boton_salir=0x7f050006;
         public static final int texto_boton_volver=0x7f050010;
@@ -68,9 +107,12 @@ public final class R {
         public static final int texto_subtitulo=0x7f050003;
         public static final int texto_subtitulo_insertar=0x7f050009;
         public static final int texto_titulo=0x7f050002;
+        public static final int texto_titulo_fallos=0x7f050016;
         public static final int texto_titulo_insertar=0x7f050008;
+        public static final int texto_titulo_resultado=0x7f050011;
         public static final int title_activity_insercion=0x7f05000c;
         public static final int title_activity_juego=0x7f05000f;
+        public static final int title_activity_resultado=0x7f050013;
     }
     public static final class style {
         /** 
